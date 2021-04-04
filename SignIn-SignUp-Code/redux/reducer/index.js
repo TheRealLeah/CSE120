ea7fbef842d3a7fe391f89b4aa4519b25ec8abbc
Gary Sun <sungarysun@gmail.com>
2021-04-03 21:31:38 -0700

    signup/login

---
+++ SignIn-SignUp-Code/redux/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import { user } from "/Users/JoshGialis/Desktop/Instagram Clone/InstagramClone/redux/reducer/user.js";
+
+const Reducers = combineReducers({
+  userState: user,
+});
+
+export default Reducers;
