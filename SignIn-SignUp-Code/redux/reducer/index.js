051032a78e3d164f2a3783ad2fd35a913b39acd4
Josue Gialis <43738543+jgialis@users.noreply.github.com>
2021-04-09 15:41:47 -0700

    Delete SignIn-SignUp-Code directory

--- SignIn-SignUp-Code/redux/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { user } from "/Users/JoshGialis/Desktop/Instagram Clone/InstagramClone/redux/reducer/user.js";
-
-const Reducers = combineReducers({
-  userState: user,
-});
-
-export default Reducers;
