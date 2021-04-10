4d68487d0055025e728f74f0c348792f8b56057d
Gary Sun <sungarysun@gmail.com>
2021-04-09 21:12:07 -0700

    Combined signup code with navigation code

---
+++ ProjectCombine/Screens/ProfileScreen.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { StyleSheet, Image } from 'react-native';
+
+import EditScreenInfo from '../Components/EditScreenInfo';
+import { Text, View } from '../Components/Themed';
+
+export default function ProfileScreen() {
+  return (
+    
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {/* <EditScreenInfo path="/screens/ProfileScreen.tsx" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
