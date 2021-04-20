e2a682261695e1fb0c40d067ce773e22ac07878c
Josh Gialis <JoshGialis@Josh-Computer.local>
2021-04-20 16:21:45 -0700

    Able to read from data base.

--- ProjectCombine/Screens/NotificationScreen.tsx
+++ ProjectCombine/Screens/NotificationScreen.tsx
@@ -9,39 +9,34 @@ import {
 } from "react-native";
 // import Divider from "react-native-elements";
 import OrangeBanner from "../Components/OrangeBanner";
-import firestore from "@react-native-firebase/firestore";
+
+import firebase from "firebase";
 
 const windowWidth = Dimensions.get("window").width;
+let data;
+let name;
+
+function Item({ name, time }) {
+  return (
+    <SafeAreaView style={x.listItem}>
+      <Text>{name}</Text>
+      <Text>{time}</Text>
+    </SafeAreaView>
+  );
+}
+
 const notifications = [
   {
-    id: 1,
+    id: "1",
     name: "You've been added to {name} event!",
     time: "31 mins ago",
   },
   {
-    id: 2,
-    name: "You've been added to {name} event!",
+    id: "2",
+    name: "You've been added to " + name + " event!",
     time: "6 hrs ago",
   },
-  {
-    id: 3,
-    name: "You've been added to {name} event!",
-    time: "21 hrs ago",
-  },
-  {
-    id: 4,
-    name: "You've been added to {name} event!",
-    time: "1 day ago",
-  },
 ];
-function Item({ name, time }) {
-  return (
-    <SafeAreaView style={x.listItem}>
-      <Text style={x.listName}>{name}</Text>
-      <Text style={x.listTime}>{time}</Text>
-    </SafeAreaView>
-  );
-}
 const x = StyleSheet.create({
   listItem: {
     backgroundColor: "cornflowerblue",
@@ -50,21 +45,14 @@ const x = StyleSheet.create({
     borderColor: "#a8c6fa",
     borderWidth: 4,
   },
-  listName: {
-    marginLeft: 100,
-    paddingTop: 20,
-    color: "white",
-    fontSize: 20,
-  },
-  listTime: {
-    marginLeft: 100,
-    paddingTop: 20,
-    paddingBottom: 5,
-    color: "cornflowerblue",
-  },
 });
 
 export default function NotificationScreen() {
+  LoadData(function () {
+    name = data["name"];
+    // console.log("After LoadData():", name);
+  });
+
   return (
     <ImageBackground
       source={require("../assets/background2.png")}
@@ -84,3 +72,161 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
+
+async function LoadData(_callback) {
+  //console.log("Before Load:");
+  var db = firebase.firestore().collection("notifications");
+  await db.get().then((querySnapshot) => {
+    // console.log("Event Size:", querySnapshot.size);
+
+    querySnapshot.forEach((documentSnapshot) => {
+      // console.log("Event ID:", documentSnapshot.id, documentSnapshot.data());
+      data = documentSnapshot.data();
+
+      // console.log("Event Name:", data['name']);
+      // console.log("Event Desc:", data['desc']);
+      // console.log("Got Data:");
+      console.log("Got Data:", data);
+    });
+  });
+  // name = data["name"];
+  // console.log("After Load:", name);
+  _callback();
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// import React, { useState, useEffect } from "react";
+// import {
+//   StyleSheet,
+//   ImageBackground,
+//   FlatList,
+//   SafeAreaView,
+//   Text,
+//   Dimensions,
+// } from "react-native";
+// import { Divider } from "react-native-elements";
+// import OrangeBanner from "../Components/OrangeBanner";
+
+// import moment from "moment";
+
+// const windowWidth = Dimensions.get("window").width;
+
+// // const notificationsCollection = firebase
+// //   .firestore()
+// //   .collection("notifications")
+// //   .doc("v6wBmuPpaIuEx0ODlarQ")
+// //   .get();
+
+// const notifications = [
+//   {
+//     id: "1",
+//     name: "YOU been added to event!",
+//     time: +new Date(),
+//   },
+//   {
+//     id: "2",
+//     name: "You've been added to event!",
+//     time: moment(+new Date()).fromNow(),
+//   },
+// ];
+
+// // async function LoadData(_callback) {
+// //   //console.log("Before Load:");
+// //   var db = firebase.firestore().collection("events");
+// //   await db.get().then((querySnapshot) => {
+// //     // console.log("Event Size:", querySnapshot.size);
+
+// //     querySnapshot.forEach((documentSnapshot) => {
+// //       //console.log("Event ID:",documentSnapshot.id, documentSnapshot.data());
+// //       data = documentSnapshot.data();
+
+// //       // console.log("Event Name:", data['name']);
+// //       // console.log("Event Desc:", data['desc']);
+// //       // console.log("Got Data:");
+// //     });
+// //   });
+// //   //name = data['name'];
+// //   // console.log("After Load:", name);
+// //   _callback();
+// // }
+
+// function Item({ name, time }) {
+//   return (
+//     <SafeAreaView style={styles.listItem}>
+//       <Text>
+//         {name} {time}
+//       </Text>
+//     </SafeAreaView>
+//   );
+// }
+
+// export default class NotificationScreen extends React.Component {
+//   state = {
+//     currentState: "blue",
+//   };
+
+//   updateState = () => this.setState({ currentState: "red" });
+
+//   render() {
+//     return (
+//       <ImageBackground
+//         source={require("../assets/background2.png")}
+//         style={styles.background}
+//       >
+//         <OrangeBanner title="Activity" />
+//         <Text
+//           onPress={this.updateState}
+//           style={{ color: this.state.currentState }}
+//         >
+//           Press to turn red
+//         </Text>
+//       </ImageBackground>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   background: {
+//     flex: 1,
+//     alignItems: "center",
+//   },
+//   listItem: { width: windowWidth },
+// });
+
+// (
+//   <ImageBackground
+//     source={require("../assets/background2.png")}
+//     style={styles.background}
+//   >
+//     <OrangeBanner title="Activity" />
+//     <FlatList
+//       ItemSeparatorComponent={() => (
+//         <Divider
+//           style={{
+//             borderColor: "dodgerblue",
+//             borderWidth: 1,
+//           }}
+//         ></Divider>
+//       )}
+//       data={notifications}
+//       renderItem={({ item }) => <Item name={item.name} time={item.time} />}
+//     />
+//   </ImageBackground>
+// );
