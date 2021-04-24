98c092b2ed409a72b4b6dbb14ecfe698c32a2b75
Gary Sun <sungarysun@gmail.com>
2021-04-24 12:33:09 -0700

    fixed styling problems for homescreen

--- ProjectCombine/Screens/EventDetails.tsx
+++ ProjectCombine/Screens/EventDetails.tsx
@@ -1,19 +1,15 @@
 import fb from "../fb";
-import * as React from "react";
-import {
-  StyleSheet,
-  ImageBackground,
-  TouchableOpacity,
-  Dimensions,
-  Button,
-} from "react-native";
+import * as React from 'react';
+import { StyleSheet, ImageBackground, TouchableOpacity, Dimensions, Button } from 'react-native';
 
-import EditScreenInfo from "../Components/EditScreenInfo";
-import { Text, View } from "../Components/Themed";
+import EditScreenInfo from '../Components/EditScreenInfo';
+import { Text, View } from '../Components/Themed';
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function EventDetails({ route, navigation }) {
+
+export default function EventDetails({route,navigation}) {
   //console.log("Params: ", route.params);
-  const { item } = route.params;
+  const {item} = route.params; 
   var name = item[0];
   var desc = item[1];
   var contactinfo = item[2];
@@ -21,101 +17,115 @@ export default function EventDetails({ route, navigation }) {
   var time = item[4];
   //console.log("Name: ", name);
   return (
-    <ImageBackground
+      <ImageBackground
       source={require("../assets/background2.png")}
       style={styles.background}
-    >
-      <Text style={styles.title}> {name} </Text>
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => console.log("PRESSED ON VOLUNTEER")}
       >
-        <Text style={styles.buttontext}>Volunteer</Text>
-      </TouchableOpacity>
-
-      <Text style={styles.desc}>Desc: {desc} </Text>
-      <Text style={styles.contact}>Contact: {contactinfo} </Text>
-      <Text style={styles.Location}>Location: {location} </Text>
-      <Text style={styles.time}>Time: {time} </Text>
-    </ImageBackground>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 25,
-    fontWeight: "bold",
-    paddingTop: 25,
-    paddingLeft: 25,
-    paddingRight: 25,
-    alignSelf: "center",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  background: {
-    flex: 1,
-    //alignItems: "center",
-  },
-  button: {
-    backgroundColor: "#E6FEFF",
-    shadowOpacity: 0.25,
-    borderRadius: 10,
-    width:
-      Dimensions.get("screen").width - Dimensions.get("screen").width * 0.6,
-    height:
-      Dimensions.get("screen").height - Dimensions.get("screen").height * 0.95,
-    marginTop:
-      Dimensions.get("screen").height - Dimensions.get("screen").height * 0.75,
-    alignSelf: "center",
-  },
-  desc: {
-    fontSize: 20,
-    fontWeight: "bold",
-    paddingTop: 15,
-    paddingLeft: 25,
-    paddingRight: 25,
-    textAlign: "left",
-  },
-  contact: {
-    fontSize: 20,
-    fontWeight: "bold",
-    paddingTop: 15,
-    paddingLeft: 25,
-    paddingRight: 25,
-    textAlign: "left",
-  },
-  Location: {
-    fontSize: 20,
-    fontWeight: "bold",
-    paddingTop: 15,
-    paddingLeft: 25,
-    paddingRight: 25,
-    textAlign: "left",
-  },
-  time: {
-    fontSize: 20,
-    fontWeight: "bold",
-    paddingTop: 15,
-    paddingLeft: 25,
-    paddingRight: 25,
-    textAlign: "left",
-  },
-  buttontext: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-    alignSelf: "center",
-    textAlignVertical: "center",
-
-    marginTop: 8,
-  },
+          <Text style={styles.title}> {name} </Text>
+          
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => console.log("PRESSED ON VOLUNTEER")}
+          >
+              <Text style={styles.buttontext}>Volunteer</Text>
+          </TouchableOpacity>
+          <View
+            style={styles.box}
+          >
+            <ScrollView>
+              <Text style={styles.time}>Time: {time} </Text>
+              <Text style={styles.Location}>Location: {location} </Text>
+              <Text style={styles.contact}>Contact: {contactinfo} </Text>
+              <Text style={styles.desc}>Description: {desc} </Text>
+            </ScrollView>
+          </View>
+          
+      </ImageBackground>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 25,
+      fontWeight: 'bold',
+      paddingTop: 25,
+      paddingLeft: 25,
+      paddingRight: 25,
+      textAlign: "center",
+    },
+    separator: {
+      marginVertical: 30,
+      height: 1,
+      width: '80%',
+    },
+    background: {
+      flex: 1,
+      //alignItems: "center",
+    },
+    button: {
+      backgroundColor: "#ffb4b0",
+      shadowOpacity: 0.1,
+      borderRadius: 10,
+      width: Dimensions.get('screen').width-(Dimensions.get('screen').width*.6),
+      height: Dimensions.get('screen').height-(Dimensions.get('screen').height*.95),
+      marginTop: Dimensions.get('screen').height-(Dimensions.get('screen').height*.75),
+      alignSelf: "center",
+    },
+    box: {
+      backgroundColor: "#E6FEFF",
+      shadowOpacity: 0.1,
+      borderRadius: 10,
+      width: Dimensions.get('screen').width-(Dimensions.get('screen').width*.1),
+      height: Dimensions.get('screen').height-(Dimensions.get('screen').height*.6),
+      marginTop: 10,
+      alignSelf: "center",
+      overflow: "scroll",
+    },
+    desc: {
+      fontSize: 20,
+      fontWeight: "bold",
+      paddingTop: 15,
+      paddingLeft: 25,
+      paddingRight: 25,
+      textAlign: "left",
+    },
+    contact: {
+      fontSize: 20,
+      fontWeight: "bold",
+      paddingTop: 15,
+      paddingLeft: 25,
+      paddingRight: 25,
+      textAlign: "left",
+    },
+    Location: {
+      fontSize: 20,
+      fontWeight: "bold",
+      paddingTop: 15,
+      paddingLeft: 25,
+      paddingRight: 25,
+      textAlign: "left",
+    },
+    time: {
+      fontSize: 20,
+      fontWeight: "bold",
+      paddingTop: 15,
+      paddingLeft: 25,
+      paddingRight: 25,
+      textAlign: "left",
+    },
+    buttontext: {
+      fontSize: 20,
+      fontWeight: "bold",
+      textAlign: "center",
+      alignSelf: "center",
+      textAlignVertical: "center",
+      
+      marginTop: 8,
+      
+  }
 });
--- ProjectCombine/Screens/HomeScreen.tsx
+++ ProjectCombine/Screens/HomeScreen.tsx
@@ -1,20 +1,17 @@
-import firebase from "firebase";
-import * as React from "react";
-import {
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
-  SafeAreaView,
-} from "react-native";
-import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 
-import { Text, View } from "../Components/Themed";
+import firebase from 'firebase';
+import * as React from 'react';
+import { StyleSheet, ImageBackground, Dimensions, SafeAreaView } from 'react-native';
+import { FlatList, ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 
-import { homedata } from "../Components/HomeData";
+import { Text, View } from '../Components/Themed';
+
+import {homedata} from "../Components/HomeData";
 
 //var testdata = [{name: "Test Name", desc:"THIS IS THE DESCRIPTION"},{name: "SECOND", desc: "NEW TEST"}];
 
 export default class HomeScreen extends React.Component {
+
   state = {
     backgroundColor: "rgba(0,0,200,0.05)",
     pressed: false,
@@ -41,89 +38,75 @@ export default class HomeScreen extends React.Component {
           ItemSeparatorComponent={this.renderSeparator}
           ListFooterComponent={this.renderSeparator}
           //keyExtractor={(item, index) => index.toString()}
+          
           renderItem={({ item }) => {
             //console.log(item);
             return (
+
               <SafeAreaView style={styles.Container}>
                 <TouchableOpacity
                   style={{
                     backgroundColor: this.state.backgroundColor,
-                    width: Dimensions.get("screen").width,
-                    height:
-                      Dimensions.get("screen").height -
-                      Dimensions.get("screen").height * 0.88,
+                    width: Dimensions.get('screen').width,
+                    height: Dimensions.get('screen').height-(Dimensions.get('screen').height*.88),
                   }}
-                  //onPress={() => console.log("PRESSED")}
-                  onPress={() =>
-                    this.props.navigation.navigate("EventDetails", { item })
-                  }
+                   //onPress={() => console.log("PRESSED")}
+                  onPress={() => this.props.navigation.navigate("EventDetails",{item})}
                 >
-                  <Text style={styles.name}>{item[0]} </Text>
-                  <Text style={styles.desc}>{item[1]} </Text>
+                  <Text style={styles.name} >{item[0]} </Text>
+                  <Text style={styles.desc} >{item[1]} </Text> 
                 </TouchableOpacity>
               </SafeAreaView>
             );
           }}
+          
         />
       </ImageBackground>
     );
   }
 }
 
-async function LoadData(_callback) {
-  //console.log("Before Load:");
-  var db = firebase.firestore().collection("events");
-  await db.get().then((querySnapshot) => {
-    // console.log("Event Size:", querySnapshot.size);
-
-    querySnapshot.forEach((documentSnapshot) => {
-      //console.log("Event ID:",documentSnapshot.id, documentSnapshot.data());
-      data = documentSnapshot.data();
-
-      // console.log("Event Name:", data['name']);
-      // console.log("Event Desc:", data['desc']);
-      // console.log("Got Data:");
-    });
-  });
-  //name = data['name'];
-  // console.log("After Load:", name);
-  _callback();
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   img: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
+    textAlign: "center",
   },
   name: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     paddingLeft: 125,
     paddingTop: 8,
     paddingBottom: 5,
     paddingRight: 5,
+    width: Dimensions.get('screen').width-(Dimensions.get('screen').width*.05),
+    height: Dimensions.get('screen').height-(Dimensions.get('screen').height*.96),
+    overflow: "hidden",
   },
   desc: {
     fontSize: 15,
-    fontWeight: "normal",
+    fontWeight: 'normal',
+    width: Dimensions.get('screen').width-(Dimensions.get('screen').width*.05),
+    height: Dimensions.get('screen').height-(Dimensions.get('screen').height*.88),
     paddingLeft: 125,
     paddingTop: 5,
     paddingBottom: 5,
     paddingRight: 5,
+    overflow: "scroll",
   },
   separator: {
     marginVertical: 10,
     height: 10,
-    width: "95%",
+    width: '95%',
     alignSelf: "center",
   },
   box: {
@@ -133,25 +116,23 @@ const styles = StyleSheet.create({
     // paddingVertical: 10,
     // paddingHorizontal: 12,
     //marginLeft: 50,
-    width:
-      Dimensions.get("screen").width - Dimensions.get("screen").width * 0.1,
-    height:
-      Dimensions.get("screen").height - Dimensions.get("screen").height * 0.88,
+    width: Dimensions.get('screen').width-(Dimensions.get('screen').width*.1),
+    height: 10,
     marginTop: 40,
     alignSelf: "center",
     //opacity: .5,
-    marginVertical: Dimensions.get("screen").height,
+    marginVertical: Dimensions.get('screen').height,
+    
   },
   background: {
     flex: 1,
     alignItems: "center",
   },
-  Container: {
-    borderWidth: 0,
-    borderColor: "dodgerblue",
+  Container: { 
+    borderWidth: 0, 
+    borderColor: "dodgerblue" ,
     alignSelf: "center",
-    width: Dimensions.get("screen").width,
-    height:
-      Dimensions.get("screen").height - Dimensions.get("screen").height * 0.8,
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height-(Dimensions.get('screen').height*.88),
   },
 });
