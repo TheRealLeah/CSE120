c912d4995132ce6ce7f009d36d12ebd1d5d6b37d
Gary Sun <sungarysun@gmail.com>
2021-04-27 12:08:18 -0700

    Volunteer Button works backend

--- ProjectCombine/Screens/EventDetails.tsx
+++ ProjectCombine/Screens/EventDetails.tsx
@@ -1,20 +1,29 @@
 import fb from "../fb";
 import * as React from 'react';
-import { StyleSheet, ImageBackground, TouchableOpacity, Dimensions, Button } from 'react-native';
+import { StyleSheet, ImageBackground, TouchableOpacity, Dimensions, Button, Alert } from 'react-native';
 
 import EditScreenInfo from '../Components/EditScreenInfo';
 import { Text, View } from '../Components/Themed';
 import { ScrollView } from "react-native-gesture-handler";
+import { notifications } from "react-native-firebase";
 
-
+var name;
+var desc;
+var contactinfo;
+var location;
+var time;
+var eventID;
+var OrgID;
 export default function EventDetails({route,navigation}) {
   //console.log("Params: ", route.params);
   const {item} = route.params; 
-  var name = item[0];
-  var desc = item[1];
-  var contactinfo = item[2];
-  var location = item[3];
-  var time = item[4];
+  name = item[0];
+  desc = item[1];
+  contactinfo = item[2];
+  location = item[3];
+  time = item[4];
+  eventID = item[5];
+  OrgID = item[6];
   //console.log("Name: ", name);
   return (
       <ImageBackground
@@ -25,7 +34,8 @@ export default function EventDetails({route,navigation}) {
           
           <TouchableOpacity
               style={styles.button}
-              onPress={() => console.log("PRESSED ON VOLUNTEER")}
+              //onPress={() => console.log("PRESSED ON VOLUNTEER")}
+              onPress={() => addEvent()}
           >
               <Text style={styles.buttontext}>Volunteer</Text>
           </TouchableOpacity>
@@ -129,3 +139,43 @@ export default function EventDetails({route,navigation}) {
       
   }
 });
+
+var getOptions = {
+  source: "myEvents"
+};
+
+function addEvent(){
+  var db = fb.firestore();
+  db.collection("users").doc(fb.auth().currentUser.uid).get().then((doc) => {
+    var temp = doc.data();
+    var data = temp["myEvents"];
+    //console.log("TESTST",data);
+    
+    if(data.indexOf(eventID) > -1){ // check if user already had this event
+      //console.log("True");
+    } else {
+      //console.log("False");
+      db.collection("users").doc(fb.auth().currentUser.uid).update( {
+        myEvents: fb.firestore.FieldValue.arrayUnion(eventID)
+      }).then(() => {
+        db.collection("notifications").add({
+        id: OrgID,
+        message: "Someone has added themselves to " + name,
+        time: 1618993824167,
+        })
+      })
+    }
+  })
+
+
+  // db.collection("users").doc(fb.auth().currentUser.uid).update( {
+  //   myEvents: fb.firestore.FieldValue.arrayUnion(eventID)
+  // }).then(() => {
+  //   db.collection("notifications").add({
+  //   id: OrgID,
+  //   message: "Someone has added themselves to " + name,
+  //   time: 1618993824167,
+  //   })
+  // })
+  
+}
