5a4cdcab001c228a0f5118c5eeb9c2b87a776193
jgialis <jgialis@ucmerced.edu>
2021-04-30 17:55:07 -0700

    Pushing Leahs volunteer event page  and Garys volunteers list  view

--- ProjectCombine/Screens/EventVolunteerScreen.js
+++ ProjectCombine/Screens/EventVolunteerScreen.js
@@ -1,10 +1,38 @@
 //LEAHS PAGE
+//imports allow for the features of react native to be useable
 import React, { Component } from "react";
-import { StyleSheet, ImageBackground, Text } from "react-native";
+//needed to import safeareaview and dimensions
+import {
+  StyleSheet,
+  ImageBackground,
+  Text,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
+//importing flatlsist (I want to use a flatlist to store everything.)
+//importing scrollview
+import {
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native-gesture-handler";
+import { homedata } from "../Components/HomeData";
 
 class EventVolunteerScreen extends Component {
-  state = {};
-
+  state = {
+    //this stuff inside is what makes the shade of blue slightly different
+    backgroundColor: "rgba(0,0,200,0.05)",
+    pressed: false,
+  };
+  //added a separator line
+  renderSeparator = () => (
+    <SafeAreaView
+      style={{
+        backgroundColor: "dodgerblue",
+        height: 1,
+      }}
+    />
+  );
   onEventAdded = (event) => {};
 
   render() {
@@ -13,19 +41,82 @@ class EventVolunteerScreen extends Component {
         source={require("../assets/background2.png")}
         style={styles.container}
       >
-        <Text style={{ fontSize: 30 }}>
-          This is the event page for volunteers
-        </Text>
+        <FlatList
+          data={homedata}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListFooterComponent={this.renderSeparator}
+          //keyExtractor={(item, index) => index.toString()}
+
+          renderItem={({ item }) => {
+            //console.log(item);
+            return (
+              <SafeAreaView style={styles.Container}>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: this.state.backgroundColor,
+                    width: Dimensions.get("screen").width,
+                    height:
+                      Dimensions.get("screen").height -
+                      Dimensions.get("screen").height * 0.88,
+                  }}
+                  //onPress={() => console.log("PRESSED")}
+                  onPress={() =>
+                    this.props.navigation.navigate("EventDetails", { item })
+                  }
+                >
+                  <Text style={styles.name}>{item[0]} </Text>
+                  <ScrollView>
+                    <Text style={styles.desc}>{item[1]} </Text>
+                  </ScrollView>
+                </TouchableOpacity>
+              </SafeAreaView>
+            );
+          }}
+        />
       </ImageBackground>
     );
   }
 }
-
+//const handles a lot of the frontend styling
 const styles = StyleSheet.create({
+  //if removed the background will dissapear
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  //img is defs neccessary or the screen will crash
+  img: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  //name causes the titles of events to be bolded and centered
+  name: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingLeft: 125,
+    paddingTop: 8,
+    paddingBottom: 20,
+    paddingRight: 5,
+    width:
+      Dimensions.get("screen").width - Dimensions.get("screen").width * 0.05,
+    height:
+      Dimensions.get("screen").height - Dimensions.get("screen").height * 0.96,
+    overflow: "hidden",
+  },
+  //added the scrolling features
+  desc: {
+    fontSize: 15,
+    fontWeight: "normal",
+    width:
+      Dimensions.get("screen").width - Dimensions.get("screen").width * 0.05,
+    height:
+      Dimensions.get("screen").height - Dimensions.get("screen").height * 0.88,
+    paddingLeft: 125,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingRight: 5,
+    overflow: "scroll",
   },
 });
 
